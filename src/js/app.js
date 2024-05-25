e4bbbf7d001f60cdfa0f225da22b3f0ae2d48ad2
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-05-26 01:21:48 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,5 @@
-import CollapsibleWidget from "./CollapsibleWidget";
-import ArticleWidget from "./ArticleWidget";
-
 let mainElement;
 
 document.addEventListener("DOMContentLoaded", () => {
   mainElement = document.querySelector("main");
-  new CollapsibleWidget(
-    mainElement,
-    (ownerElement) => new ArticleWidget(ownerElement)
-  );
 });
