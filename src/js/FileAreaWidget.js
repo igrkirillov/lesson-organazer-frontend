79d7a4b9f861b4e8b894e97b036db2e31bc2ceab
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-06-23 19:26:22 +0300

    commit

--- src/js/FileAreaWidget.js
+++ src/js/FileAreaWidget.js
@@ -4,6 +4,7 @@ export default class FileAttachmentsWidget {
     this.application = application;
     this.element = this.createElement(ownerElement);
     this.typingAreaWidget = typingAreaWidget;
+    this.files = [];
     this.addListeners();
   }
 
@@ -25,15 +26,28 @@ export default class FileAttachmentsWidget {
     }
   }
 
+  onRemoveAttachmentClick(fileName, event) {
+    event.preventDefault();
+    console.log(event);
+  }
+
   addFile(file) {
     const element = document.createElement("div");
     element.classList.add("file-attachment");
+    element.dataset.name = file.name;
     element.innerHTML = `
       <img src="${fileIcon}" class="file-attachment-icon">
       <span>${file.name}</span>
       <a href="#" class="file-attachment-remove" title="Удалить">X</a>
     `;
     this.element.appendChild(element);
-    return element;
+    this.files.push(file);
+
+    element.querySelector(".file-attachment-remove")
+      .addEventListener("click", this.onRemoveAttachmentClick.bind(this, file.name));
+  }
+
+  clear() {
+
   }
 }
