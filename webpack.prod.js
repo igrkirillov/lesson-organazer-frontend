b3a91a33809f377899f81f99a35e2880abe47537
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-06-27 12:12:41 +0300

    commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const webpack = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
